6e3f2363a2dbc7a519457ae718ebe4621fdbfe75
Shreyansh Sharma <shreyanshsharma1011@gmail.com>
2026-05-24 01:24:49 -0700

    feat: add output and file size limits

--- src/constants.ts
+++ src/constants.ts
@@ -6,3 +6,15 @@ export const EXCLUDE_DIRS = new Set([
   ".next",
   "coverage",
 ]);
+
+export const TREE_EXCLUDE_DIRS = new Set([...EXCLUDE_DIRS, ".cursor"]);
+
+export const MAX_OUTPUT_BYTES = 30_720;
+
+export const MAX_FILE_BYTES = 512 * 1024;
+
+export const MAX_OUTLINE_FILE_BYTES = 256 * 1024;
+
+export const MAX_SYMBOL_BLOCK_LINES = 40;
+
+export const MAX_WALK_FILES = 5000;
